5d474c3ecc10696c785dc7cc56536e2f386bafd0
rickleck <rickleck@users.noreply.github.com>
2022-09-15 23:44:23 +0200

    navigation data config, temp pages, container padding

--- 03_week37_bootstrap/js/main.js
+++ 03_week37_bootstrap/js/main.js
@@ -1,8 +1,34 @@
-import "./templates.js"
+import "./templates.js";
 
-//Render navigation
+//Check active page
+const activePage = (url) => {
+    return window.location.href.includes(url);
+};
+
+//Create navigation data
+const navData = {
+    pages: [
+        {
+            name: "Home",
+            url: "index.html",
+            active: activePage("index.html"),
+        },
+        {
+            name: "About Us",
+            url: "about.html",
+            active: activePage("about.html"),
+        },
+        {
+            name: "Contact",
+            url: "contact.html",
+            active: activePage("contact.html"),
+        },
+    ],
+};
+
+//Render navigation template
 try {
-    const template = Handlebars.templates.navigation();
+    const template = Handlebars.templates.navigation(navData);
     document.querySelector("header").insertAdjacentHTML("afterbegin", template);
 } catch (error) {
     console.log("Could not add navigation template.");
