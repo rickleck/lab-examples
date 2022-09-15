6a52896b2675d5366591076ef4487ac554f00311
rickleck <rickleck@users.noreply.github.com>
2022-09-15 17:37:31 +0200

    added handlebars funtionality

--- 03_week37_bootstrap/js/main.js
+++ 03_week37_bootstrap/js/main.js
@@ -1,10 +1,11 @@
-// Import the Bootstrap bundle
-//
-// This includes Popper and all of Bootstrap's JS plugins.
+//Render navigation
+const template = Handlebars.templates.navigation();
+try {
+    document.querySelector("header").insertAdjacentHTML("afterbegin", template);
+} catch (error) {
+    console.log("Could not add navigation template.");
+    console.log(error);
+}
 
+//Bootstrap include
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
-
-//
-// Place any custom JS here
-//
