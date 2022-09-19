17614a14e8cc207c185167e1b0ecf33a59f028cd
rickleck <rickleck@users.noreply.github.com>
2022-09-19 23:43:34 +0200

    formatting

--- 04_week38_basics_js/main.js
+++ 04_week38_basics_js/main.js
@@ -23,9 +23,7 @@ if (isBrowser) log("node main.js");
 log("*** Lab 4 Week 38: Basics - JavaScript ***");
 log(num1 + " plus " + num2 + " är: " + (num1 + num2));
 log(num1 + " minus " + num2 + " är: " + (num1 - num2));
-
 log(num1 + " gånger " + num2 + " är: " + num1 * num2);
-
 log(
     "Hej, mitt namn är " +
         person.fname +
