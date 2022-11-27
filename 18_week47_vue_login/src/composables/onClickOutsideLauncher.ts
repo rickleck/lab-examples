74c23f6fbe1a4c3c1af5f4d1c9c0e07384113e3d
rickleck <rickleck@users.noreply.github.com>
2022-11-27 14:32:30 +0100

    formatting

--- 18_week47_vue_login/src/composables/onClickOutsideLauncher.ts
+++ 18_week47_vue_login/src/composables/onClickOutsideLauncher.ts
@@ -1,10 +1,10 @@
 import { onClickOutside } from '@vueuse/core';
 import type { Ref } from 'vue';
 
-function onClickOutsideLauncher(element: Ref, launches: string, callBack: Function) {
+function onClickOutsideLauncher(element: Ref, launchId: string, callBack: Function) {
     onClickOutside(element, (e: PointerEvent) => {
         const target = e.target as HTMLElement;
-        if (target.dataset.launches && target.dataset.launches.includes(launches)) return;
+        if (target.dataset.launches && target.dataset.launches.includes(launchId)) return;
         callBack();
     });
 }
