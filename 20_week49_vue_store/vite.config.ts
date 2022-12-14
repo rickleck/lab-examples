012c835d81ef18fc9122d920af4be22152c3093d
rickleck <rickleck@users.noreply.github.com>
2022-12-14 01:07:15 +0100

    deploy

--- 20_week49_vue_store/vite.config.ts
+++ 20_week49_vue_store/vite.config.ts
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), vsharp()],
+    plugins: [vue()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
