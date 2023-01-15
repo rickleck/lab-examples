a7e468fa853e456bb8bc8834be589021ebbe2317
rickleck <rickleck@users.noreply.github.com>
2023-01-15 20:13:22 +0100

    new build, one vendor chunk

--- 22_week01_react_todo_list/vite.config.ts
+++ 22_week01_react_todo_list/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
+import { splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), splitVendorChunkPlugin()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -15,14 +16,5 @@ export default defineConfig({
     },
     build: {
         chunkSizeWarningLimit: 1000,
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    firestore: ['@firebase/firestore', 'reactfire'],
-                    react: ['react', 'react-dom'],
-                    dndkit: ['@dnd-kit/core', '@dnd-kit/modifiers', '@dnd-kit/sortable'],
-                },
-            },
-        },
     },
 });
